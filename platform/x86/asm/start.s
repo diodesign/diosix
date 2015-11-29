fd19b7c72967ab09b2dd5a9c277979e3a58a2da8
Chris Williams <diodesign@gmail.com>
2015-11-29 07:00:42 +0000

    [x86] Added video text printing for early debugging

--- platform/x86/asm/start.s
+++ platform/x86/asm/start.s
@@ -19,19 +19,22 @@ bits 32
 ; We need to initialize a known good environment.
 ;
 start:
-; print 'GO' to screen to show we've been booted
-  mov dword [0xb8000], 0x2f4f2f47
-
 ; give us a stack to play with
   mov esp, boot_stack_top
+
+; clear the screen and let the user know we're alive
+  call boot_video_cls
+  mov edx, boot_welcome_msg
+  call boot_video_writeln
 
 ; perform some preflight tests to make sure this is a sane environment
   call test_multiboot_present
   call test_cpuid_present
   call test_x86_64_present
 
-; print 'OK' to screen to acknowledge we've got this far
-  mov dword [0xb8000], 0x2f4b2f4f
+; acknowledge we've got this far
+  mov edx, boot_tests_complete_msg
+  call boot_video_writeln
   hlt
 
 ; -------------------- preflight checks ----------------------
@@ -48,8 +51,9 @@ test_multiboot_present:
   jne .no_multiboot
   ret
 .no_multiboot:
-  mov al, "0"
+  mov edx, boot_error_no_multiboot
   jmp early_boot_err
+
 
 ; test_cpuid_present
 ; 
@@ -73,8 +77,9 @@ test_cpuid_present:
   jz .no_cpuid		; if not, bail out to error handler
   ret			; if yes, then let's get out of here
 .no_cpuid:
-  mov al, "1"
+  mov edx, boot_error_no_cpuid
   jmp early_boot_err
+
 
 ; test_x86_64_present
 ;
@@ -91,28 +96,108 @@ test_x86_64_present:
   jz .no_x86_64		; if not, then it isn't available so bail out
   ret			; if yes, then let's go
 .no_x86_64:
-  mov al, "2"
+  mov edx, boot_error_no_64bit
   jmp early_boot_err
+
+
+; ------------------- basic video code --------------------
+
+; boot_video_cls
+;
+; Clear the screen and reset the cursor to the top left
+; corner of the display. Assumes screen is an 80 x 25 text display
+;
+boot_video_cls:
+  push eax
+  push ebx
+  mov eax, 80 * 25
+  mov ebx, 0xb8000
+  
+.cls_loop:
+  mov word [ebx], 0x2720  ; write a blank grey-on-green space
+  add ebx, 2
+  dec eax
+  jnz .cls_loop           ; loop through all screen words
+
+  ; reset line position
+  mov byte [boot_video_line_nr], 0
+
+  pop ebx
+  pop eax
+  ret
+
+
+; boot_video_writeln
+;
+; Write a null-terminated string to screen. A newline is automatically added.
+; Don't forget that each character is stored as two bytes - the first is the
+; color byte, and the second is the ASCII code.
+; => edx = pointer to string in memory
+; <= edx = incrememnted to end of string
+;
+boot_video_writeln:
+  push eax
+  push ebx
+  
+  ; calculate start of line in memory based on current text line index
+  xor eax, eax
+  mov ebx, 80
+  mov al, byte [boot_video_line_nr]
+  mul bl
+  shl eax, 1
+  add eax, 0xb8000	; eax = (index * 80 * 2) + 1 + 0xb8000 base
+
+.writeln_loop:
+  mov bl, byte [edx]
+  cmp bl, 0
+  jz .writeln_loop_done ; bail out when we hit the null term
+  mov byte [eax], bl
+  add edx, 1
+  add eax, 2
+  jmp .writeln_loop
+
+.writeln_loop_done:
+  xor eax, eax
+  mov al, byte [boot_video_line_nr]
+  add eax, 1
+  mov byte [boot_video_line_nr], al ; increment line index
+
+  pop ebx
+  pop eax
+  ret
+
+; -------------------- boot messages ----------------------
+
+boot_welcome_msg:
+  db "Woo - it's diosix! Now performing preflight checks...",0
+boot_tests_complete_msg:
+  db "Tests complete. Congrats, this system is good to boot.",0
+
+boot_error_no_multiboot:
+  db "Oh no. The boot loader isn't Multiboot compatible. Sorry! Halting.",0
+boot_error_no_cpuid:
+  db "Oh no. This machine's processor is too old to boot. Sorry! Halting.",0
+boot_error_no_64bit:
+  db "Oh no. This machine's processor doesn't support 64-bit x86. Halting.",0
 
 ; -------------------- error handler ----------------------
 
 ; early_boot_err
 ;
-; Print an error code on screen if something goes
-; wrong unexpectedly during first stages of boot
-; => al = error character to print
+; Print an error message on the screen and halt the CPU
+; => edx = error string to print
 ; <= never returns
 ;
 early_boot_err:
-  mov dword [0xb8000], 0x4f524f45 ; write 'ERR:  ' to screen
-  mov dword [0xb8004], 0x4f3a4f52
-  mov dword [0xb8008], 0x4f204f20
-  mov byte  [0xb800a], al	  ; overwrite last char with error code
+  call boot_video_writeln
   hlt
 
-
 ; reserve a tiny stack while bringing up the system
+; and variables for writing to the screen
 section .bss
+
+boot_video_line_nr: resb 1
+
 boot_stack_bottom:
   resb 64
 boot_stack_top:
