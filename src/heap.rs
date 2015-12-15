d2af307e70ed2daab53a2723bb069577e631d30a
Chris Williams <diodesign@gmail.com>
2015-12-15 23:08:53 +0000

    Tighten up dynamic allocator's accounting, also clarify comments and variable names to distinguish between a block (a unit of memory) and an allocation (a group of one or more blocks that fulfill an allocation request)

--- src/heap.rs
+++ src/heap.rs
@@ -46,31 +46,32 @@ const BLOCK_SIZE:       usize = 32; /* see above discussion for block size */
 const BLOCK_MAGIC_IN_USE: u64 = 0x4c69766548656170; /* LiveHeap */
 const BLOCK_MAGIC_FREE:   u64 = 0x4465616448656170; /* DeadHeap */
 
-/* there are a maximum of 128 x 32-byte blocks in a 4K page, minus space for the header */
-const BLOCKS_PER_PAGE:  usize = (::hardware::physmem::SMALL_PAGE_SIZE - HEADER_SIZE) / BLOCK_SIZE;
+const BLOCK_NULL_PTR: *mut HeapAllocation = 0 as *mut HeapAllocation;
 
 pub static KERNEL: Mutex<Heap> = Mutex::new(Heap
                                     {
-                                        free: 0 as *mut _,
-                                        blocks_in_use:     0,
-                                        free_pool_blocks:  0,
-                                        total_bytes_requested:   0,
-                                        total_alloc_requests: 0,
+                                        free:                  BLOCK_NULL_PTR,
+                                        blocks_in_use:         0,
+                                        allocations_in_use:    0,
+                                        free_pool_blocks:      0,
+                                        total_bytes_requested: 0,
+                                        total_alloc_requests:  0,
                                     });
 
 pub struct Heap
 {
-    /* pointer to free list */
+    /* pointer to head of the free list */
     free: *mut HeapAllocation,
 
     /* usage stats - not including allocations' headers */
     blocks_in_use: usize, /* blocks in use right now */
     free_pool_blocks: usize, /* blocks sitting in the free pool */
+    allocations_in_use: usize, /* number of allocations */
 
     /* diagnostic stats so we can calculate the
      * average size of the kernel's allocs. */
     total_bytes_requested: usize, /* running total of bytes allocated */
-    total_alloc_requests: usize, /* running total of alloc requests */
+    total_alloc_requests: usize, /* running total of allocatio requests */
 }
 
 /* change HEADER_SIZE if you change this structure */
@@ -99,16 +100,18 @@ impl Heap
 {
     /* top_up_free_pool
      *
-     * Dump a page of RAM into the pool to provide some free blocks.
-     * <= error code on failure.
+     * Add a page of RAM to the pool to provide some free blocks.
+     * <= returns an error code on failure.
      */
     fn top_up_free_pool(&mut self) -> Result<(), KernelInternalError>
     {
         /* grab a page to add to the free list to get us started */
         let free_block = try!(::hardware::physmem::get_page());
-        
-        self.add_to_free(free_block, ::hardware::physmem::SMALL_PAGE_SIZE);
-        self.free_pool_blocks = self.free_pool_blocks + BLOCKS_PER_PAGE;
+       
+        /* the page is headerless, so add it using add_raw_mem_to_free(),
+         * which gives us a count of usable blocks in the memory area. */
+        let block_count = self.add_raw_mem_to_free(free_block, ::hardware::physmem::SMALL_PAGE_SIZE);
+        self.free_pool_blocks = self.free_pool_blocks + block_count;
 
         Ok(())
     }
@@ -124,14 +127,17 @@ impl Heap
     {
         if size == 0 { return Err(KernelInternalError::HeapBadAllocReq); }
 
+        /* calculate nr of blocks required to fulfill this alocation request,
+         * not including the header */
         let mut blocks_req: usize = size / BLOCK_SIZE;
         if(size % BLOCK_SIZE) > 0
         {
             blocks_req = blocks_req + 1; /* round up to nearest whole block */
         }
 
-        let mut block = self.free;
-        let mut block_size = 0;
+        /* start search for suitable group of free block(s) to fulfill request */
+        let mut found = self.free; /* search forward through the free list */
+        let mut found_blocks = 0; /* size of found block */
 
         /* make two passes: first time looking at the free pool, and
          * second time after priming the pool with new blocks */
@@ -141,90 +147,89 @@ impl Heap
             loop
             {
                 /* give up when we hit a null pointer */
-                if block == 0 as *mut _ { break; }
+                if found == BLOCK_NULL_PTR { break; }
             
                 unsafe
                 {
                     /* sanity check */
-                    if (*block).magic != BLOCK_MAGIC_FREE
+                    if (*found).magic != BLOCK_MAGIC_FREE
                     {
-                        kprintln!("[mem] BUG! Non-free heap block {:p} in free pool", block);
+                        kprintln!("[mem] BUG! Non-free heap block {:p} in free pool", found);
                         return Err(KernelInternalError::HeapCorruption);
                     }
 
-                    /* is this block big enough? */
-                    block_size = (*block).blocks;
-                    if block_size >= blocks_req { break; }
+                    /* is this allocation of free block(s) big enough for the request? */
+                    found_blocks = (*found).blocks;
+                    if found_blocks >= blocks_req { break; }
 
-                    /* try the next block */
-                    block = (*block).next;
+                    /* try the next block if the group of free block(s) wasn't big enough */
+                    found = (*found).next;
                 }
             }
 
-            /* if we're coming up short in our search, reflll the pool and try again */
-            if block == 0 as *mut _
-            {
-                try!(self.top_up_free_pool());
-            }
-            else
-            {
-                break;
-            }
+            /* end the search if we've found a suitable group of block(s) */
+            if found != BLOCK_NULL_PTR { break; }
 
-            block = self.free;
+            /* if we're still here, reflll the pool and try again */
+            try!(self.top_up_free_pool());
+            found = self.free;
         }
 
         /* if we're still drawing dead, then bail out - the request is probably too big */
-        if block == 0 as *mut _ { return Err(KernelInternalError::HeapBadAllocReq); }
+        if found == BLOCK_NULL_PTR { return Err(KernelInternalError::HeapBadAllocReq); }
 
-        /* we may have to split a group of blocks */
-        if block_size > blocks_req
+        /* we may have to split a group of blocks: snap off the unneeded part of the 
+         * group and put it back into the free pool with a new header. the current
+         * header is going to be repurposed for the allocated block(s). */
+        if found_blocks > blocks_req
         {
-            let split_addr = (block as usize) + HEADER_SIZE + (blocks_req * BLOCK_SIZE);
-            self.add_to_free(split_addr, (block_size - blocks_req) * BLOCK_SIZE);
+            let split_addr = (found as usize) + HEADER_SIZE + (blocks_req * BLOCK_SIZE);
+            self.free_pool_blocks = self.free_pool_blocks - found_blocks;
+            let added_blocks = self.add_raw_mem_to_free(split_addr, (found_blocks - blocks_req) * BLOCK_SIZE);
+            self.free_pool_blocks = self.free_pool_blocks + added_blocks;
         }
 
-        /* detach the block from the free list */
+        /* detach the found group of block(s) from the free list */
         unsafe
         {
             /* remove from head of the linked list */
-            if self.free == block
+            if self.free == found
             {
-                self.free = (*block).next;
+                self.free = (*found).next;
             }
             
             /* connect the previous block in the chain to the next block */
-            let mut previous = (*block).previous;
-            if previous != 0 as *mut _
+            let mut previous = (*found).previous;
+            if previous != BLOCK_NULL_PTR
             {
-                (*previous).next = (*block).next;
-                (*block).previous = 0 as *mut _;
+                (*previous).next = (*found).next;
+                (*found).previous = BLOCK_NULL_PTR;
             }
 
             /* connect the next block in the chain to the previous block */
-            let mut next = (*block).next;
-            if next != 0 as *mut _
+            let mut next = (*found).next;
+            if next != BLOCK_NULL_PTR
             {
-                (*next).previous = (*block).previous;
-                (*block).next = 0 as *mut _;
+                (*next).previous = (*found).previous;
+                (*found).next = BLOCK_NULL_PTR;
             }
 
             /* update block metadata */
-            (*block).magic = BLOCK_MAGIC_IN_USE;
-            (*block).blocks = blocks_req;
+            (*found).magic = BLOCK_MAGIC_IN_USE;
+            (*found).blocks = blocks_req;
         }
 
-        /* the easy part - fill out the statistics */
+        /* the easy part: update the accounting */
         self.blocks_in_use = self.blocks_in_use + blocks_req;
+        self.allocations_in_use = self.allocations_in_use + 1;
         self.total_alloc_requests = self.total_alloc_requests + 1;
         self.total_bytes_requested = self.total_bytes_requested + size;
-        self.free_pool_blocks = self.free_pool_blocks - blocks_req;
 
         /* skip over the header when handing back a pointer */
-        Ok(((block as usize) + HEADER_SIZE) as *mut _)
+        Ok(((found as usize) + HEADER_SIZE) as *mut _)
     }
 
-    /* add_to_free
+    /* add_raw_mem_to_free
      *
      * Add a headerless-block (or group of them) to the free pool.
      * This does not update the heap's accounting variables because
@@ -232,17 +237,21 @@ impl Heap
      * to the caller to update the heap's accounting.
      * => ptr = address of start of block(s)
      *    size = number of bytes in group
+     * <= number of usable blocks in size
      */
-    fn add_to_free(&mut self, ptr: usize, size: usize)
+    fn add_raw_mem_to_free(&mut self, ptr: usize, size: usize) -> usize
     {
         let mut new = ptr as *mut HeapAllocation;
         let usable_blocks = (size - HEADER_SIZE) / BLOCK_SIZE;
+
+        /* don't put an empty space into the free pool */
+        if size == 0 { return 0; }
 
         unsafe
         {
             /* add a header */
             (*new).next = self.free;
-            (*new).previous = 0 as *mut _;
+            (*new).previous = BLOCK_NULL_PTR;
             (*new).blocks = usable_blocks;
             (*new).magic = BLOCK_MAGIC_FREE;
 
@@ -250,12 +259,14 @@ impl Heap
             self.free = new;
 
             /* make old head of the list point back to this new block */
-            if (*new).next != 0 as *mut _
+            if (*new).next != BLOCK_NULL_PTR
             {
                 let mut old_head = (*new).next;
                 (*old_head).previous = new;
             }
         }
+
+        return usable_blocks;
     }
 
     pub fn debug_stats(&self)
@@ -267,21 +278,28 @@ impl Heap
         }
 
         kprintln!("[mem] kernel heap statistics:");
-        kprintln!("... {} bytes allocated in {} blocks", self.blocks_in_use * BLOCK_SIZE, self.blocks_in_use);
-        kprintln!("... {} bytes in free pool in {} blocks", self.free_pool_blocks * BLOCK_SIZE, self.free_pool_blocks);
+        
+        kprintln!("... {} bytes in {} allocations, {} blocks in use, plus {} bytes overhead",
+                  self.blocks_in_use * BLOCK_SIZE, self.allocations_in_use, self.blocks_in_use,
+                  HEADER_SIZE * self.allocations_in_use);
+        
+        kprintln!("... {} bytes in free pool in {} blocks",
+                  self.free_pool_blocks * BLOCK_SIZE, self.free_pool_blocks);
+       
         kprintln!("... {} allocation rquests, {} bytes requested, average request size is {} bytes",
-                  self.total_alloc_requests, self.total_bytes_requested, self.total_bytes_requested / self.total_alloc_requests);
+                  self.total_alloc_requests, self.total_bytes_requested,
+                  self.total_bytes_requested / self.total_alloc_requests);
 
         /* walk the free list */
         kprintln!("[mem] kernel heap free pool:");
         let mut block = self.free;
         loop
         {
-            if block == 0 as *mut _ { break; }
+            if block == BLOCK_NULL_PTR { break; }
             unsafe
             {
-                kprint!("... {} blocks [previous {:x} next {:x}] ",
-                          (*block).blocks, (*block).previous as usize, (*block).next as usize);
+                kprint!("... {} blocks [{:x} <--- {:x} --> {:x}] ",
+                          (*block).blocks, (*block).previous as usize, block as usize, (*block).next as usize);
                 if (*block).magic == BLOCK_MAGIC_FREE
                 {
                     kprintln!("[good magic]");
