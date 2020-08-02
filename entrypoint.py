1dfec537de941e32a13905a2aab7352439961bd3
Chris Williams <diodesign@tuta.io>
2020-08-02 05:00:03 -0700

    Debug Google Cloud Run support

--- entrypoint.py
+++ entrypoint.py
@@ -23,13 +23,13 @@ from flask import Flask
 if __name__ == "__main__":
     if (os.environ.get('K_SERVICE')) != '':
         print('Running HTTP service for Google Cloud')
-        app = Flask(__name__)
-        @app.route('/')
-        def ContainerService():
-            return 'Container built. Use docker images and docker run in the Google Cloud shell to run this container.\n'
-        app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
+        # app = Flask(__name__)
+        # @app.route('/')
+        # def ContainerService():
+        #   return 'Container built. Use docker images and docker run in the Google Cloud shell to run this container.\n'
+        # app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
     else:
         print('Running locally')
-        stream = os.popen('. $HOME/.cargo/env && cd /build/diosix && {}'.format(' '.join(sys.argv[1:])))
-        output = stream.read()
-        output
+        # stream = os.popen('. $HOME/.cargo/env && cd /build/diosix && {}'.format(' '.join(sys.argv[1:])))
+        # output = stream.read()
+        # output
