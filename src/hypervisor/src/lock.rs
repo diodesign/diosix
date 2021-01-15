bd1a7518eb1aec35f40e147b2b1476f36163f6bc
Chris Williams <diodesign@tuta.io>
2021-01-15 04:30:59 -0800

    Overhaul mutex * Use a spin lock to assign an owner to a mutex. if an owner is assigned, the mutex is considered held. if an owner is not assigned, the mutex is available. * Allow physical cores to reenter mutexes they hold. * Name mutexes to aid in debugging * Add basic deadlock detection * Provide a spin lock primitive

--- src/hypervisor/src/lock.rs
+++ src/hypervisor/src/lock.rs
@@ -1,16 +1,19 @@
-/* diosix high-level hypervisor's mutex primitive
+/* diosix high-level hypervisor's locking primitives
  *
- * Provide reentrant mutex locks for critical
- * data structures. this means when a physical
- * core holds a lock and then tries to lock it
+ * Provides a standard spin lock and a mutex
+ * 
+ * The mutex is reentrant, which means when a physical
+ * core holds a mutex and then tries to acquire it
  * again, this operation will succeed.
  * 
- * this is so that if a core holds a lock
+ * this is so that, eg, if a core holds a mutex
  * and is interrupted, it can regain access
  * to the locked data, use it, and release it.
  * 
- * use lock() to acquire an exclusive lock
- * drop the lock to release it
+ * use lock() to acquire a mutex.
+ * it is unlocked when it goes out of scope.
+ * the mutex also maintains accounting stats
+ * and is named to aid debugging.
  * 
  * (c) Chris Williams, 2021.
  *
@@ -20,100 +23,159 @@
 use core::sync::atomic::{AtomicBool, AtomicUsize, Ordering};
 use core::cell::UnsafeCell;
 use core::ops::{Deref, DerefMut};
+use super::pcore::PhysicalCore;
 
-pub struct Mutex<T>
+/* if a lock() call spins more than DEADLOCK_THRESHOLD times
+   then it's considered a deadlocked mutex */
+const DEADLOCK_THRESHOLD: usize = 1000000;
+
+/* define a snip lock primitive */
+pub struct SpinLock
 {
-    acquired: AtomicBool,
-    owner: AtomicUsize,
-    lock_attempts: AtomicUsize,
-    lock_count: AtomicUsize,
-    release_count: AtomicUsize,
-    regain_count: AtomicUsize,
-    content: UnsafeCell<T>,
+    lock: AtomicBool
 }
 
-/* Mutex uses the same API as std's Mutex. Create a Mutex using new() and then
-   call lock() to block until lock successfully acquired. Drop the lock to release
-   it. Use try_lock() to make one attempt at acquiring the lock */
-
-impl<T> Mutex<T>
+impl SpinLock
 {
-    pub fn new(data: T) -> Mutex<T>
+    pub fn new() -> SpinLock
     {
-        Mutex
-        {
-            acquired: AtomicBool::new(false),
-            owner: AtomicUsize::new(0),
-            lock_attempts: AtomicUsize::new(0),
-            lock_count: AtomicUsize::new(0),
-            release_count: AtomicUsize::new(0),
-            regain_count: AtomicUsize::new(0),
-            content: UnsafeCell::new(data),
-        }
+        SpinLock { lock: AtomicBool::new(false) }
     }
 
-    pub fn lock(&self) -> MutexGuard<'_, T>
+    /* spin until the lock value == must_equal, and then atomically do lock value = new_value */
+    fn spin(&self, must_equal: bool, new_value: bool)
     {
-        /* id 0 is no owner, so shift up IDs by one */
-        let id = super::pcore::PhysicalCore::get_id() + 1;
-
-        /* spin trying to set acquired to true to obtain the mutex */
         loop
         {
-            self.lock_attempts.fetch_add(1, Ordering::Relaxed);
-
-            if self.acquired.compare_exchange(false, true, Ordering::Acquire, Ordering::Relaxed) == Ok(false)
+            if self.lock.compare_exchange(must_equal, new_value, Ordering::Acquire, Ordering::Relaxed) == Ok(must_equal)
             {
-                /* available locks should not have a non-zero owner */
-                if self.owner.load(Ordering::SeqCst) != 0
-                {
-                    hvdebug!("BUG: Lock {:p} was available yet owned by {}", &self, self.owner.load(Ordering::SeqCst));
-                }
-
-                /* we've acquired the lock. store our ownership in it */
-                self.owner.store(id, Ordering::SeqCst);
-                self.lock_count.fetch_add(1, Ordering::Relaxed);
-                break;
+                return;
             }
-
-            /* couldn't acquire lock.. but it's not ours, is it? */
-            if self.owner.load(Ordering::SeqCst) == id
-            {
-                /* we own this lock -- ensure it remains locked for us */
-                self.acquired.store(true, Ordering::SeqCst);
-                self.regain_count.fetch_add(1, Ordering::Relaxed);
-                break;
-            }
-        }
-
-        MutexGuard { mutex: &self }
+        }   
     }
     
-    /* return true if the lock is held, or false if not */
-    pub fn is_locked(&self) -> bool
+    /* acquire the lock, and block until successful */
+    pub fn lock(&self)
     {
-        self.acquired.load(Ordering::SeqCst)
+        self.spin(false, true);
     }
 
     /* release the lock */
+    pub fn unlock(&self)
+    {
+        self.spin(true, false);   
+    }
+}
+
+pub struct Mutex<T>
+{
+    /* the data we're protecting */
+    content: UnsafeCell<T>,
+
+    /* owner_lock protects owned and owner.
+       if the owned is false, then the mutex is considered free.
+       if the owned is true, the mutex is considered held by a physical core whose ID == owner */
+    owner_lock: SpinLock,
+    owned: AtomicBool,
+    owner: AtomicUsize,
+
+    /* accounting */
+    lock_attempts: AtomicUsize,
+    lock_count: AtomicUsize,
+    description: &'static str
+}
+
+/* Mutex uses the same API as std's Mutex. Create a Mutex using new() and then
+   call lock() to block until mutex successfully acquired. Drop the mutex to release */
+impl<T> Mutex<T>
+{
+    pub fn new(description: &'static str, data: T) -> Mutex<T>
+    {
+        Mutex
+        {
+            content: UnsafeCell::new(data),
+            owner_lock: SpinLock::new(),
+            owned: AtomicBool::new(false),
+            owner: AtomicUsize::new(0),
+            lock_attempts: AtomicUsize::new(0),
+            lock_count: AtomicUsize::new(0),
+            description
+        }
+    }
+
+    /* spin until ready to return reference to protected data */
+    pub fn lock(&self) -> MutexGuard<'_, T>
+    {
+        let mut attempts = 0;
+
+        let this_pcore_id = PhysicalCore::get_id();
+        loop
+        {
+            /* hold the spin lock while checking the metadata */
+            self.owner_lock.lock();
+            self.lock_attempts.fetch_add(1, Ordering::Relaxed);
+            attempts = attempts + 1;
+            if attempts == DEADLOCK_THRESHOLD
+            {
+                hvdebug!("BUG: {} mutex ({:p}) may be deadlocked", self.description, &self.content);
+            }
+
+            /* determine if the mutex is available, or may even
+               already be held by this physical core */
+            if self.owned.load(Ordering::SeqCst) == false
+            {
+                /* lock is available so claim it */
+                self.owned.store(true, Ordering::SeqCst);
+                self.owner.store(this_pcore_id, Ordering::SeqCst);
+                break;
+            }
+            else
+            {
+                /* mutex is already held though this pcore may own it anyway */
+                if self.owner.load(Ordering::SeqCst) == this_pcore_id
+                {
+                    break;
+                }
+            }
+
+            /* give another core a chance to acquire the mutex */
+            self.owner_lock.unlock();
+        }
+
+        /* don't forget to unlock the metadata
+           before returning a reference to the content */
+        self.lock_count.fetch_add(1, Ordering::Relaxed);
+        self.owner_lock.unlock();
+        MutexGuard { mutex: &self }
+    }
+
+    /* unlock the mutex */
     fn unlock(&self)
     {
-        self.owner.store(0, Ordering::SeqCst);
-        self.release_count.fetch_add(1, Ordering::Relaxed);
-        self.acquired.store(false, Ordering::Release);
+        self.owner_lock.lock();
+        self.owned.store(false, Ordering::SeqCst);
+        self.owner_lock.unlock();
+    }
+
+    /* return true if the mutex is locked, or false if not */
+    pub fn is_locked(&self) -> bool
+    {
+        self.owner_lock.lock();
+        let locked = self.owned.load(Ordering::SeqCst);
+        self.owner_lock.unlock();
+        locked
     }
 }
 
 /* pretty print a mutex's stats */
-impl<T> core::fmt::Debug for Mutex<T>
+impl<T> core::fmt::Debug for MutexGuard<'_, T>
 {
     fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result
     {
-        write!(f, "{:p}: {} lock attempts, locked {}, released {}, regained lock {} times", &self,
-            self.lock_attempts.load(Ordering::Relaxed),
-            self.lock_count.load(Ordering::Relaxed),
-            self.release_count.load(Ordering::Relaxed),
-            self.regain_count.load(Ordering::Relaxed))
+        write!(f, "{} attempts to acquire {}, {} succeeded",
+            self.mutex.lock_attempts.load(Ordering::Relaxed),
+            self.mutex.description,
+            self.mutex.lock_count.load(Ordering::Relaxed))
     }
 }
 
