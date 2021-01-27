df4d73bc691e5129fb13b63a1582fb4e34932b64
Chris Williams <diodesign@tuta.io>
2021-01-27 01:55:43 -0800

    Use an enum to define service types

--- src/hypervisor/src/service.rs
+++ src/hypervisor/src/service.rs
@@ -8,11 +8,24 @@
 use super::lock::Mutex;
 use hashbrown::hash_map::{HashMap, Entry};
 use alloc::collections::vec_deque::VecDeque;
+use alloc::vec::Vec;
 use super::message;
 use super::error::Cause;
 use super::capsule::{self, CapsuleID};
 
-pub type ServiceID = usize;
+/* available type of services that can be offered by a capsule */
+#[derive(Clone, Copy, PartialEq, Eq, Hash)]
+pub enum ServiceType
+{
+    ConsoleInterface = 0 /* act as the console interface manager */
+}
+
+/* select either a particular service or all services */
+pub enum SelectService
+{
+    AllServices,
+    SingleService(ServiceType)
+}
 
 /* todo: a fixed list of known system services,
 such as video, sound, serial, network, etc
@@ -23,7 +36,14 @@ to access those underlying resources. */
 /* maintain a table of registered services */
 lazy_static!
 {
-    static ref SERVICES: Mutex<HashMap<ServiceID, Service>> = Mutex::new("system service table", HashMap::new());
+    static ref SERVICES: Mutex<HashMap<ServiceType, Service>> = Mutex::new("system service table", HashMap::new());
+}
+
+/* return true if the given service type is registered */
+pub fn is_registered(stype: ServiceType) -> bool
+{
+    let tbl = SERVICES.lock();
+    tbl.contains_key(&stype)
 }
 
 /* describe an individual service */
@@ -39,24 +59,26 @@ impl Service
     {
         self.msgs.push_front(msg);
     }
+
+    pub fn get_capsule_id(&self) -> CapsuleID { self.capsuleid }
 }
 
-/* register a service for a capsule. this will fail if the service is
-   already registered or if the capsule has no right to run the service
-   or if the capsule doesn't exist. 
-    => sid = ID of service to register
+/* register a service for a capsule. this will fail if the
+   capsule has no right to run the service, or if the capsule doesn't exist,
+   or if another capsule has already claimed the service type.
+   be aware if the capsule has already claimed the service, it will
+   return Err(Cause::ServiceAlreadyOwner). this will be the result if a
+   restarted capsule registers its service(s) again. services aren't released
+   during a restart to provide a non-stop continuation of services.
+    => stype = type of service to register
        cid = ID of capsule to handle this service
     <= return Ok for success, or a failure code */
-pub fn register(sid: ServiceID, cid: CapsuleID) -> Result<(), Cause>
+pub fn register(stype: ServiceType, cid: CapsuleID) -> Result<(), Cause>
 {
-    match capsule::is_service_allowed(cid, sid)
+    if capsule::is_service_allowed(cid, stype)? == false
     {
-        Some(flag) => if flag == false
-        {
-            return Err(Cause::ServiceNotAllowed);
-        },
-        None => return Err(Cause::CapsuleBadID)
-    };
+        return Err(Cause::ServiceNotAllowed);
+    }
 
     let service = Service
     {
@@ -64,40 +86,72 @@ pub fn register(sid: ServiceID, cid: CapsuleID) -> Result<(), Cause>
         msgs: VecDeque::new()
     };
 
-    /* ensure we do not double register a service */
-    if let Entry::Vacant(v) = SERVICES.lock().entry(sid)
+    match SERVICES.lock().entry(stype)
     {
-        v.insert(service);
-        return Ok(());
+        Entry::Vacant(v) =>
+        {
+            v.insert(service);
+        },
+        Entry::Occupied(o) => if o.get().get_capsule_id() != cid
+        {
+            /* another capsule owns this service */
+            return Err(Cause::ServiceAlreadyRegistered);
+        }
+        else
+        {
+            /* this capsule already owns this service */
+            return Err(Cause::ServiceAlreadyOwner)
+        }
     }
-    else
-    {
-        return Err(Cause::ServiceAlreadyRegistered)
-    }
+
+    Ok(())
 }
 
-/* deregister a service so that its capsule is no longer responsible for it
-   => sid = service to deregister
+/* deregister one or all services belonding to a capsule
+   so that it is no longer responsible for them
+   => stype = service to deregister, or None for all of them
+      cid = ID of capsule to strip of its services
    <= Ok for success, or an error code for failure */
-pub fn deregister(sid: ServiceID) -> Result<(), Cause>
+pub fn deregister(stype: SelectService, cid: CapsuleID) -> Result<(), Cause>
 {
-    match SERVICES.lock().remove(&sid)
+    let mut tbl = SERVICES.lock();
+    let mut to_remove = Vec::new();
+
+    for (registered, owner) in (&tbl).iter()
     {
-        None => Err(Cause::ServiceNotFound),
-        _ => Ok(())
+        /* remove either everything that matches the capsule ID, or a particular service */
+        match stype
+        {
+            SelectService::AllServices => if owner.get_capsule_id() == cid
+            {
+                to_remove.push(*registered);
+            },
+            SelectService::SingleService(s) => if owner.get_capsule_id() == cid && s == *registered
+            {
+                to_remove.push(*registered);
+            }
+        }
     }
+
+    /* now remove the vicims */
+    for victim in to_remove
+    {
+        tbl.remove(&victim);
+    }
+
+    Ok(())
 }
 
 /* send the given message msg to a registered service */
 pub fn send(msg: message::Message) -> Result<(), Cause>
 {
-    let sid = match msg.get_receiver()
+    let stype = match msg.get_receiver()
     {
-        message::Recipient::Service(sid) => sid,
+        message::Recipient::Service(stype) => stype,
         _ => return Err(Cause::MessageBadType)
     };
 
-    if let Some(service) = SERVICES.lock().get_mut(&sid)
+    if let Some(service) = SERVICES.lock().get_mut(&stype)
     {
         service.queue(msg);
         Ok(())
