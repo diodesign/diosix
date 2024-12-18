d8d9cf3c0253d2f8293a8de77497bce6259fa845
diodesign <chrisw@diosix.org>
2024-12-18 04:29:27 -0800

    Split assembly code off into separate files, using a top-level .s to bring them together

---
+++ hw/qemu/top.s
@@ -0,0 +1,6 @@
+# Top-level file for support assembly code for Qemu-compatible systems
+#
+# Copyright (c) 2024 Chris Williams <chrisw@diosix.org>
+# SPDX-License-Identifier: MIT
+
+.include "hw/qemu/entry.s"
